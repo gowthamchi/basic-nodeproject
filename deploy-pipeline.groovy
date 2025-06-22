c59472ed18e3468cd69ac6c757250f0698caec24
Goutham Reddy <your-email@example.com>
2025-06-22 21:55:04 +0530

    deleteing some files

---
+++ deploy-pipeline.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+        stage("Git Checkout") {
+            steps {
+                git url: 'https://github.com/gowthamchi/basic-nodeproject.git'
+            }
+        }
+
+        stage("Docker Build") {
+            steps {
+                sh 'docker build -t nodeimage .'
+            }
+        }
+
+        stage("Docker Push") {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: 'dockerlogin',
+                    usernameVariable: 'USERNAME',
+                    passwordVariable: 'PASSWORD'
+                )]) {
+                    sh '''
+                    echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
+                    docker tag nodeimage gowtham1198/nodeimage:latest
+                    docker push gowtham1198/nodeimage:latest
+                    '''
+                }
+            }
+        }
+
+        stage("Docker Run") {
+            steps {
+                sh 'docker run -d -p 3000:3000 gowtham1198/nodeimage:latest'
+            }
+        }
+    }
+}
